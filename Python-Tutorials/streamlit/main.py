c3c74ad2b40e06580d288a07f7fea3416f5d2c0c
Reine0017 <m.fortitudo.fr@gmail.com>
2021-12-18 01:00:18 +0900

    image resizer

--- Python-Tutorials/streamlit/main.py
+++ Python-Tutorials/streamlit/main.py
@@ -1,11 +1,17 @@
 import streamlit as st 
-import pandas as pd
+import imutils
 from PIL import Image
+import numpy as np
 
-#st.write("""# My First App""")
-st.title("My First App")
+st.title("My Streamlit Image Resizer")
 
 content_file = st.sidebar.file_uploader("Choose a Content Image", type=["png", "jpg", "jpeg"])
-if content_file is not None:
+
+imgWidth = st.sidebar.number_input('Resized Image Width', 1, step=1)
+
+if content_file is not None and imgWidth>1:
   content = Image.open(content_file)
-  st.image(content, channels='BGR', clamp=True)
+  content = np.array(content)
+  content = imutils.resize(content, width=imgWidth)
+
+  st.image(content, clamp=True)
